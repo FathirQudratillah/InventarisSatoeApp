d9daba527b5e99cb1438428b03d81e19ec8cca82
Razif Arba <razifmakan@gmail.com>
2026-02-04 21:11:45 +0700

    tambahan koe barang di pemeliharaan barang

--- database/migrations/2026_02_02_070229_create_pemeliharaan_barang_table.php
+++ database/migrations/2026_02_02_070229_create_pemeliharaan_barang_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('pemeliharaan_barang', function (Blueprint $table) {
             $table->char('id_pemeliharaan', 8)->primary();
+            $table->char('kode_barang', 11);
             $table->string('kegiatan_pemeliharaan', 32);
             $table->date('tanggal_pemeliharaan');
             $table->text('keterangan');
