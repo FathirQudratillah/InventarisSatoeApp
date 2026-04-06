34bca821b7ff1c88edbd748d528dea405a1d4ced
Thirrrq_ <fathirqudratillah@gmail.com>
2026-04-06 12:07:08 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -118,7 +118,7 @@ Route::middleware(['auth', 'role:siswa,guru'])
     ->group(function () {
         Route::get('/user', [DashboardController::class, 'index'])->name('dashboard.user');
         Route::get('/peminjaman-barang/create', [PeminjamanBarangController::class, 'create'])->name('user.peminjaman-barang.create');
-        Route::post('/peminjaman-barang.store', [PeminjamanBarangController::class, 'store'])->name('user.peminjaman-barang.store');
+        Route::post('/peminjaman-barang/store', [PeminjamanBarangController::class, 'store'])->name('user.peminjaman-barang.store');
         route::get('/peminjaman/{id}/back', [PeminjamanBarangController::class, 'back'])
             ->name('peminjaman-barang.back');
     });
